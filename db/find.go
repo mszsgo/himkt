9e032221ddd0c008ac925b2113eec10fa78a8134
Zengs <zengs@himkt.cn>
2020-07-25 16:05:27 +0800

    update

--- db/find.go
+++ db/find.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -21,8 +22,30 @@ func Find(c *mongo.Collection, result interface{}, ctx context.Context, filter i
 	return
 }
 
+// @deprecated 改为使用  FindPage
 func Page(c *mongo.Collection, result interface{}, ctx context.Context, filter interface{}, opts ...*options.FindOptions) (total int64, err error) {
 	err = Find(c, result, ctx, filter, opts...)
+	if err != nil {
+		return 0, err
+	}
+	return c.CountDocuments(ctx, filter)
+}
+
+// 分页查询
+func FindPage(ctx context.Context, c *mongo.Collection, filter interface{}, sort bson.M, pageSize, pageNum int64, result interface{}) (total int64, err error) {
+	if sort == nil {
+		sort = bson.M{}
+	}
+	if sort["_id"] == nil {
+		sort["_id"] = -1 // 默认根据_id倒序
+	}
+	if pageNum == 0 {
+		pageNum = 1
+	}
+	if pageSize == 0 {
+		pageSize = 20
+	}
+	err = Find(c, result, ctx, filter, options.Find().SetSort(sort).SetSkip(pageSize*(pageNum-1)).SetLimit(pageSize))
 	if err != nil {
 		return 0, err
 	}
