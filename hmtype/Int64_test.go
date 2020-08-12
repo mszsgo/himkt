46dabe82a4fb08d7fddbd71ebc58a26245bb83d3
Zengs <zengs@himkt.cn>
2020-08-12 16:27:26 +0800

    update hmtype.Int64

---
+++ hmtype/Int64_test.go
@@ -0,0 +1,27 @@
+package hmtype
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type TJson struct {
+	A Int64 `json:"a"`
+}
+
+func TestInt64_UnmarshalJSON(t *testing.T) {
+	s := `{"a":"123123"}`
+	var j *TJson
+	err := json.Unmarshal([]byte(s), &j)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	t.Log(j.A)
+	rj, err := json.Marshal(j)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	t.Log(string(rj))
+}
