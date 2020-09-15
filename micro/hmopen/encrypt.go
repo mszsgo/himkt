0f4ccfbd5c08b62667ffd9053a0ba39118dc06fc
Zengs <zengs@himkt.cn>
2020-09-15 09:29:00 +0800

    update

---
+++ micro/hmopen/encrypt.go
@@ -0,0 +1,63 @@
+package hmopen
+
+import (
+	"himkt/hm"
+)
+
+var (
+	amap map[string]string
+)
+
+func getEncryptSecret(appid string) (string, error) {
+	secret := amap[appid]
+	if secret != "" {
+		return secret, nil
+	}
+	r, err := GetOpenApp(appid, false)
+	if err != nil {
+		return "", err
+	}
+	amap[appid] = r.DesKey
+	secret = r.DesKey
+	return secret, nil
+}
+
+// DES+MD5接口报文加密
+func EncryptDesMd5(s, appid string) (v string, err error) {
+	key, err := getEncryptSecret(appid)
+	if err != nil {
+		return
+	}
+	v, err = hm.EncryptDesMd5(s, key)
+	if err == nil {
+		return
+	}
+
+	amap[appid] = ""
+	key, err = getEncryptSecret(appid)
+	if err != nil {
+		return
+	}
+	v, err = hm.EncryptDesMd5(s, key)
+	return
+}
+
+// DES+MD5接口报文解密
+func DecryptDesMd5(s, appid string) (v string, err error) {
+	key, err := getEncryptSecret(appid)
+	if err != nil {
+		return
+	}
+	v, err = hm.DecryptDesMd5(s, key)
+	if err == nil {
+		return
+	}
+
+	amap[appid] = ""
+	key, err = getEncryptSecret(appid)
+	if err != nil {
+		return
+	}
+	v, err = hm.DecryptDesMd5(s, key)
+	return
+}
